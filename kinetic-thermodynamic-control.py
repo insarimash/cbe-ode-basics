8d1d62b69458dd9b86b552e017a241115cb2e39d
Insar <imashinsar@gmail.com>
2026-01-15 17:19:06 +0900

    renamed files, added basic kinetic-thermodynamic control of the reaction using Arrhenius equation

--- kinetic-thermodynamic-control.py
+++ kinetic-thermodynamic-control.py
@@ -1,24 +1,39 @@
+# this is for A = B(thermodynamic favorite) A = C(kinetic favorite) reactions
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
+R = 8.314
+T = 300
+A = 1000
 
-k1 = 1
-k2 = 1
+Ea1b = 60000
+Ea2b = 100000
+
+Ea1c = 30000
+Ea2c = 50000
+
+k1b = A*np.exp(-Ea1b/(R*T))
+k2b = A*np.exp(-Ea2b/(R*T))
+
+k1c = A*np.exp(-Ea1c/(R*T))
+k2c = A*np.exp(-Ea2c/(R*T))
 
 
-conc0 = np.array([1, 1, 0])
+conc0 = np.array([1, 0, 0])
 
 def dfdt(t, conc):
     A, B, C = conc
-    r1 = k1*A*A*B
-    r2 = k2*C
-    dAdt = 2*(-r1 + r2)
-    dBdt = -r1 + r2
-    dCdt = r1 - r2
+    r1b = k1b*A
+    r2b = k2b*B
+    r1c = k1c*A
+    r2c = k2c*C
+    dAdt = - r1b - r1c + r2b + r2c
+    dBdt = r1b - r2b
+    dCdt = r1c - r2c
     return [dAdt, dBdt, dCdt]
 
 conc = solve_ivp(fun = dfdt, 
-                 t_span=(0, 10), 
+                 t_span=(0, 100), 
                  y0 = conc0,
                  )
 
@@ -29,7 +44,7 @@ plt.plot(conc.t, conc.y[2], label='C(t)', color='green')
 
 plt.xlabel('Time (t)')
 plt.ylabel('Values')
-plt.title('A+B = C reaction somulation')
+plt.title('kinetic-thermodynamic control simulation')
 plt.legend() # This shows the labels we defined in plt.plot
 plt.grid(True)
 plt.show()
